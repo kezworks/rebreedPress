aa71872c38408f21886effeef89ab004708ed925
kezworks-pc <kezworks@gmail.com>
2018-12-27 16:48:35 +0900

    add imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,11 @@ const autoprefixer    = require('gulp-autoprefixer');
 const packageImporter = require('node-sass-package-importer');
 const browserSync     = require('browser-sync');
 const plumber         = require("gulp-plumber");
+const imagemin        = require('gulp-imagemin');
+const imageminJpg     = require('imagemin-mozjpeg');
+const imageminGif     = require('imagemin-gifsicle');
+const imageminPng     = require('imagemin-pngquant');
+const imageminSvg     = require('imagemin-svgo');
 
 // SASS
 gulp.task('sass', function(done) {
@@ -27,8 +32,22 @@ gulp.task('sass', function(done) {
       cascade: false
     }))
     .pipe(gulp.dest('./dist/assets/css/'));
-    done();
+  done();
 });
+
+// IMAGE-COMPRESSION
+gulp.task('image-compression', function(done) {
+  gulp.src('./src/assets/images/**/*+(jpg|jpeg|png|gif)')
+    .pipe(imagemin([
+      imageminPng(),
+      imageminJpg(),
+      imageminGif(),
+      imageminSvg()
+    ]))
+    .pipe(gulp.dest( './dist/assets/images/' ));
+  done();
+});
+
 
 // BROWSER SYNC
 gulp.task('browser-sync', function(done) {
