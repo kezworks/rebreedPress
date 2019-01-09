a7cd4f39ffca619c34fbb2dddef98d8001971161
kezworks-pc <kezworks@gmail.com>
2019-01-09 12:14:50 +0900

    190109 更新分

--- page-templates/parts/breadcrumbs.php
+++ page-templates/parts/breadcrumbs.php
@@ -1,8 +1,1 @@
-<div class="m-breadcrumbs">
-  <div class="l-inner">
-    <ol>
-      <li><a href="<?php echo site_url(); ?>/">トップページ</li>
-      <li><a href="<?php echo the_permalink(); ?>"><?php echo the_title(); ?></a></li>
-    </ol>
-  </div>
-</div>
+<?php breadcrumbs(); ?>
